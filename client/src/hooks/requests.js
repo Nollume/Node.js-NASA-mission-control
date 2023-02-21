bc27d6f94b32b65fe9eeebc31855a0e4ba28d07d
Nollume <marek.dorotovic@gmail.com>
2023-02-21 13:01:04 +0100

    pipeline workflow

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,18 +1,21 @@
-const API_URL = "http://localhost:8000/v1";
+const API_URL = 'v1';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
-  return response.json();
-  // Load planets and return as JSON.
+  return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
-  const fetchLaunches = await response.json();
-  return fetchLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
-  // Load launches, sort by flight number, and return as JSON.
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
@@ -22,27 +25,30 @@ async function httpSubmitLaunch(launch) {
       },
       body: JSON.stringify(launch),
     });
-  } catch (error) {
-    console.log(error);
+  } catch(err) {
     return {
       ok: false,
     };
   }
-  // Submit given launch data to launch system.
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
       method: "delete",
     });
-  } catch (error) {
-    console.log(error);
+  } catch(err) {
+    console.log(err);
     return {
       ok: false,
     };
   }
-  // Delete launch with given ID.
 }
 
-export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
